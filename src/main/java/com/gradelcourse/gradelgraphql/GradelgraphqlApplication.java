8c9507d43c9fdef68c8ae9aec5a908ee179881a2
Matthew Hess <mhess@equinix.com>
2023-08-10 16:22:31 -0700

    init

---
+++ src/main/java/com/gradelcourse/gradelgraphql/GradelgraphqlApplication.java
@@ -0,0 +1,13 @@
+package com.gradelcourse.gradelgraphql;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GradelgraphqlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GradelgraphqlApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gradelcourse/gradelgraphql/GradelgraphqlApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gradelcourse.gradelgraphql;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GradelgraphqlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
