ed595af7c50f753c6af3d2672ac07f12c0b46b62
Matthew Hess <mhess@equinix.com>
2023-08-14 09:55:27 -0700

    Added 'Book' type and dependent types, along with 'book' query to schema. Implemented data resolver and data faker classes for handling book-related data.

---
+++ src/main/java/com/gradelcourse/gradelgraphql/component/fake/FakeBookDataResolver.java
@@ -0,0 +1,26 @@
+package com.gradelcourse.gradelgraphql.component.fake;
+
+import com.gradelcourse.gradelgraphql.datasource.FakeBookDataSource;
+import com.gradelcourse.gradelgraphql.generated.types.Book;
+import com.netflix.graphql.dgs.DgsComponent;
+import com.netflix.graphql.dgs.DgsData;
+import com.netflix.graphql.dgs.InputArgument;
+import org.apache.commons.lang3.StringUtils;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
+
+@DgsComponent
+public class FakeBookDataResolver {
+
+    @DgsData(parentType = "Query", field = "books")
+    public List<Book> booksWrittenBy(@InputArgument(name = "author") Optional<String> authorName) {
+        if (authorName.isEmpty() || StringUtils.isBlank(authorName.get())) {
+            return FakeBookDataSource.BOOK_LIST;
+        }
+        return FakeBookDataSource.BOOK_LIST.stream()
+                .filter(book -> StringUtils.containsIgnoreCase(book.getAuthor().getName(), authorName.get()))
+                .collect(Collectors.toList());
+    }
+}
