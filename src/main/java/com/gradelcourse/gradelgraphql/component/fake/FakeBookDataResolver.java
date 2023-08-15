70f13b408c81223de7132084cff19b6daa454469
Matthew Hess <mhess@equinix.com>
2023-08-14 17:02:32 -0700

    Implemented Data resolver using dgs codegen plugin

--- src/main/java/com/gradelcourse/gradelgraphql/component/fake/FakeBookDataResolver.java
+++ src/main/java/com/gradelcourse/gradelgraphql/component/fake/FakeBookDataResolver.java
@@ -1,13 +1,19 @@
 package com.gradelcourse.gradelgraphql.component.fake;
 
 import com.gradelcourse.gradelgraphql.datasource.FakeBookDataSource;
+import com.gradelcourse.gradelgraphql.generated.DgsConstants;
 import com.gradelcourse.gradelgraphql.generated.types.Book;
+import com.gradelcourse.gradelgraphql.generated.types.ReleaseHistory;
+import com.gradelcourse.gradelgraphql.generated.types.ReleaseHistoryInput;
 import com.netflix.graphql.dgs.DgsComponent;
 import com.netflix.graphql.dgs.DgsData;
 import com.netflix.graphql.dgs.InputArgument;
+import graphql.schema.DataFetchingEnvironment;
 import org.apache.commons.lang3.StringUtils;
 
 import java.util.List;
+import java.util.Map;
+import java.util.Objects;
 import java.util.Optional;
 import java.util.stream.Collectors;
 
@@ -23,4 +29,25 @@ public class FakeBookDataResolver {
                 .filter(book -> StringUtils.containsIgnoreCase(book.getAuthor().getName(), authorName.get()))
                 .collect(Collectors.toList());
     }
+
+    @DgsData(
+            parentType = DgsConstants.QUERY_TYPE,
+            field = DgsConstants.QUERY.BooksByReleased
+    )
+    public List<Book> getBooksByReleased(DataFetchingEnvironment dataFetchingEnvironment) {
+        var releasedMap = (Map<String, Object>) dataFetchingEnvironment
+                .getArgument(DgsConstants.QUERY.BOOKSBYRELEASED_INPUT_ARGUMENT.ReleasedInput);
+        var releasedInput = ReleaseHistoryInput.newBuilder()
+                .printedEdition((boolean) releasedMap.get(DgsConstants.RELEASEHISTORYINPUT.PrintedEdition))
+                .year((int) releasedMap.get(DgsConstants.RELEASEHISTORYINPUT.Year)).build();
+
+        return FakeBookDataSource.BOOK_LIST.stream()
+                .filter(book -> this.matchReleaseHistory(releasedInput, book.getReleased()))
+                .collect(Collectors.toList());
+    }
+
+    public boolean matchReleaseHistory(ReleaseHistoryInput input, ReleaseHistory element) {
+        return input.getPrintedEdition().equals(element.getPrintedEdition()) && Objects.equals(input.getYear(), element.getYear());
+    }
+
 }
